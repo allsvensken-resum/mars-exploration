607fa88a0a55414be0051ff4b0969066b39d8c28
suppakorn <suppakorn.resume@gmail.com>
2022-05-15 22:47:38 +0700

    feat: add move backward feature

--- services/rover.go
+++ services/rover.go
@@ -67,5 +67,6 @@ func getMoveInstructionMapper() map[string][2]int {
 	moveMap["L"] = [2]int{0, -1}
 	moveMap["R"] = [2]int{0, 1}
 	moveMap["F"] = [2]int{1, 0}
+	moveMap["B"] = [2]int{-1, 0}
 	return moveMap
 }
