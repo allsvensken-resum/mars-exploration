a9bad4169905359abc29170b73ae6bdeff218992
suppakorn <suppakorn.resume@gmail.com>
2022-05-16 00:16:45 +0700

    feat: add new instruction HL and HR on mapper

--- services/rover.go
+++ services/rover.go
@@ -65,7 +65,9 @@ func isMoveInstruction(instruction int) bool {
 func getMoveInstructionMapper() map[string][2]int {
 	moveMap := make(map[string][2]int)
 	moveMap["L"] = [2]int{0, -2}
+	moveMap["HL"] = [2]int{0, -1}
 	moveMap["R"] = [2]int{0, 2}
+	moveMap["HR"] = [2]int{0, 1}
 	moveMap["F"] = [2]int{1, 0}
 	moveMap["B"] = [2]int{-1, 0}
 	return moveMap
